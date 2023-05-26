73712c17627e1b2a8c4dcfef7a873db974e1f89b
yjchen <yanjen.chen4453@gmail.com>
2023-05-27 01:57:57 +0800

    Update test

--- test.cc
+++ test.cc
@@ -1,10 +1,13 @@
 #include <gpcpp.h>
 
+#include <chrono>
 #include <iostream>
 
 int main() {
-    gpcpp::Rectangle rect1(0, 0, 10, 10, 0);
-    gpcpp::Rectangle rect2(5, 5, 10, 10, 45);
+    // start timing
+    auto start = std::chrono::high_resolution_clock::now();
+    gpcpp::Rectangle rect1(5, 5, 8, 6, 0);
+    gpcpp::Rectangle rect2(7, 7, 10, 10, 44);
     gpcpp::Polygon result;
 
     gpcpp::Intersect(rect1, rect2, result);
@@ -12,11 +15,17 @@ int main() {
         auto centroid = result.centroid();
         std::cout << "Area: " << result.area() << std::endl;
         std::cout << "Centroid: " << centroid.first << ", " << centroid.second << std::endl;
+        std::cout << "Vertices: ";
+        result.PrintVertices();
         std::cout << "Num contours: " << result.num_contours() << std::endl;
         std::cout << "Num strips: " << result.num_strips() << std::endl;
     } else {
         std::cout << "No intersection" << std::endl;
     }
-
+    // end timing
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "Time: "
+              << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " us"
+              << std::endl;
     return 0;
 }
