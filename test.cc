579f1e76b0e032620e9b01451bb34134528a700c
yjchen <yanjen.chen4453@gmail.com>
2023-05-29 00:22:53 +0800

    Update test code

--- test.cc
+++ test.cc
@@ -7,26 +7,47 @@
 int main() {
     // start timing
     auto start = std::chrono::high_resolution_clock::now();
-    fcpc::Rectangle clip_rect(5, 5, 10, 10, 0);
-    fcpc::Rectangle subj_rect(5, 15, 10, 10, 0);
-    fcpc::Polygon result, work_area;
-    fcpc::SuthHodgClip(subj_rect, clip_rect, result, work_area);
+    double r1[] = {-6, -3, 7, 7, 0};
+    double r2[] = {-6, -3, 7, 7, 50};
 
-    /* gpcpp::Rectangle clip_rect(5, 5, 8, 6, 0);
-    gpcpp::Rectangle subj_rect(7, 7, 10, 10, 44);
-    gpcpp::Polygon result;
-    gpcpp::Intersect(subj_rect, clip_rect, result); */
-    if (!result.is_empty()) {
-        auto centroid = result.centroid();
-        std::cout << "Area: " << result.area() << std::endl;
-        std::cout << "Centroid: " << centroid.x << ", " << centroid.y << std::endl;
-        /* std::cout << "Centroid: " << centroid.first << ", " << centroid.second << std::endl; */
-        std::cout << "Vertices: ";
-        result.PrintVertices();
-        /* std::cout << "Num contours: " << result.num_contours() << std::endl;
-        std::cout << "Num strips: " << result.num_strips() << std::endl; */
-    } else {
-        std::cout << "No intersection" << std::endl;
+    {
+        fcpc::Rectangle clip_rect(r1[0], r1[1], r1[2], r1[3], r1[4]);
+        fcpc::Rectangle subj_rect(r2[0], r2[1], r2[2], r2[3], r2[4]);
+        fcpc::Polygon result, work_area;
+        fcpc::SuthHodgClip(subj_rect, clip_rect, result, work_area);
+        fcpc::SuthHodgClip(subj_rect, clip_rect, result, work_area);
+
+        std::cout << "=== fcpc ===" << std::endl;
+        if (!result.is_empty()) {
+            auto centroid = result.centroid();
+            std::cout << "Area: " << result.area() << std::endl;
+            std::cout << "Centroid: " << centroid.x << ", " << centroid.y << std::endl;
+            std::cout << "Vertices: ";
+            result.PrintVertices();
+        } else {
+            std::cout << "No intersection" << std::endl;
+        }
+    }
+
+    std::cout << std::endl;
+    {
+        gpcpp::Rectangle clip_rect(r1[0], r1[1], r1[2], r1[3], r1[4]);
+        gpcpp::Rectangle subj_rect(r2[0], r2[1], r2[2], r2[3], r2[4]);
+        gpcpp::Polygon result;
+        gpcpp::Intersect(subj_rect, clip_rect, result);
+
+        std::cout << "=== gpcpp ===" << std::endl;
+        if (!result.is_empty()) {
+            auto centroid = result.centroid();
+            std::cout << "Area: " << result.area() << std::endl;
+            std::cout << "Centroid: " << centroid.first << ", " << centroid.second << std::endl;
+            std::cout << "Vertices: ";
+            result.PrintVertices();
+            std::cout << "Num contours: " << result.num_contours() << std::endl;
+            std::cout << "Num strips: " << result.num_strips() << std::endl;
+        } else {
+            std::cout << "No intersection" << std::endl;
+        }
     }
     // end timing
     auto end = std::chrono::high_resolution_clock::now();
